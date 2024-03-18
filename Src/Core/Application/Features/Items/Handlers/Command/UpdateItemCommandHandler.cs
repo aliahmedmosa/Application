dadfd38da5110f18721c29a238f8d2e2df91ece4
aliahmedmosa1@gmail.com <aliahmedmosa1@gmail.com>
2024-03-18 22:28:24 +0200

    Upload project

---
+++ Src/Core/Application/Features/Items/Handlers/Command/UpdateItemCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Items.Requests.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Items.Handlers.Command
+{
+    public class UpdateItemCommandHandler : IRequestHandler<UpdateItemCommand, Unit>
+    {
+        private readonly IItemRepository _repository;
+        private readonly IMapper _mapper;
+
+        public UpdateItemCommandHandler(IItemRepository repository,IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(UpdateItemCommand request, CancellationToken cancellationToken)
+        {
+            var oldItem = await _repository.GetAsync(request.ItemDTO.Id);
+            var res = _mapper.Map(request.ItemDTO, oldItem);
+            await _repository.UpdateAsync(res);
+            return Unit.Value;
+        }
+    }
+}
